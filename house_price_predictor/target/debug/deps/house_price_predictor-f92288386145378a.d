d9c0fd489b3c269beeed20550a84dd361b40a20d
Sameer Ahmad <samimughal7786@gmail.com>
2024-11-23 11:51:22 +0500

    Added a new Project Structure

---
+++ house_price_predictor/target/debug/deps/house_price_predictor-f92288386145378a.d
@@ -0,0 +1,5 @@
+D:\ML_using_rust\lets_ml_in_rust\house_price_predictor\target\debug\deps\house_price_predictor-f92288386145378a.exe: src\main.rs
+
+D:\ML_using_rust\lets_ml_in_rust\house_price_predictor\target\debug\deps\house_price_predictor-f92288386145378a.d: src\main.rs
+
+src\main.rs:
---
+++ house_price_predictor/target/debug/house_price_predictor.d
@@ -0,0 +1,1 @@
+D:\ML_using_rust\lets_ml_in_rust\house_price_predictor\target\debug\house_price_predictor.exe: D:\ML_using_rust\lets_ml_in_rust\house_price_predictor\src\main.rs
